ec95e41406317ed12acf6d38f163a57a94c90d5a
ironoa <alessio.onori@gmail.com>
2022-03-11 16:24:21 +0100

    upgraded dependencies, imported claimer

--- config/env.sample.complete.sh
+++ config/env.sample.complete.sh
@@ -23,3 +23,13 @@ export MATRIXBOT_ROOM_ID='!xxx:matrix.org'
 export LETSENCRYPT_ACME_SERVER='https://acme-v02.api.letsencrypt.org/directory'
 export LETSENCRYPT_ACME_EMAIL='xxx@gmail.com'
 export LETSENCRYPT_SOLVER_CLOUDFLARE_EMAIL='xxx@gmail.com'
+
+export LOKI_DOMAIN="example.com"
+
+#### Claimer
+
+export KUSAMA_CLAIMER_PASSWORD='pass'
+export KUSAMA_CLAIMER_WALLET='{"address":"5EZj5n2epr2J1sVe2PhNJhJJ8JHZ8jdcQ7iyrbzkpguCDQxb","encoded":"ZOu66lvbZQ1OilENNSxTNiLmcjUeCxnvD7G3NSP2ClYAgAAAAQAAAAgAAAC2nWTkigv/aP7FgS8GM4v7szpTan7+DSfo6VJRqyDty6HWNbWo+3Zr2i1zC8l/gNTHUEm5Iz+lZiLo9GEk17yVDDLLI1Dn5N+oSCrm1wTsm2r4zxem5v9ZU/vdsNqXyDakvAELagtWFKwWxQaROnHn74Wzw9Uu73TCnQUUu+kRFnXS4DXnJF0nskQrxqPYwDO4xvBZtcWppIS6of94","encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{"name":"trash test code","whenCreated":1600669656080}}'
+
+export POLKADOT_CLAIMER_PASSWORD='pass'
+export POLKADOT_CLAIMER_WALLET='{"address":"5EZj5n2epr2J1sVe2PhNJhJJ8JHZ8jdcQ7iyrbzkpguCDQxb","encoded":"ZOu66lvbZQ1OilENNSxTNiLmcjUeCxnvD7G3NSP2ClYAgAAAAQAAAAgAAAC2nWTkigv/aP7FgS8GM4v7szpTan7+DSfo6VJRqyDty6HWNbWo+3Zr2i1zC8l/gNTHUEm5Iz+lZiLo9GEk17yVDDLLI1Dn5N+oSCrm1wTsm2r4zxem5v9ZU/vdsNqXyDakvAELagtWFKwWxQaROnHn74Wzw9Uu73TCnQUUu+kRFnXS4DXnJF0nskQrxqPYwDO4xvBZtcWppIS6of94","encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},"meta":{"name":"trash test code","whenCreated":1600669656080}}'
